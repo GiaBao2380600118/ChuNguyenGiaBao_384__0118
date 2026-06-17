769bfce109941e2383ecf53d3fafe6afb8df39f5
GiaBao2380600118 <bingucci15112000@gmail.com>
2026-06-17 15:55:16 +0700

    clean: keep only lab-06 caesar_app and ecc_app

--- lab-05/ssl/certificates/make-cert.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Generating SSL Key and Certificate...
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -new -x509 -keyout server-key.key -out server-cert.crt -days 365 -config server-cert.cnf -nodes
-echo Certificate generation completed.
